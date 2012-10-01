be19ccbf0301cedefce53561dea6606a63ed3654
Thanasis Doumas <tdoumas@gmail.com>
2012-10-01 16:04:10 +0300

    Extended to support input elements

--- jquery.remove-upcase-accents.js
+++ jquery.remove-upcase-accents.js
@@ -37,7 +37,7 @@ jQuery.extend(jQuery.expr[":"], {
 
 jQuery.extend({
 	removeAcc: function( elem ) {
-		var text = elem.innerHTML;
+		var text = (elem.tagName.toLowerCase() == "input") ? elem.value : elem.innerHTML;
 
 		text = text.replace( /Ά/g, "Α" );
 		text = text.replace( /ά/g, "α" );
@@ -60,7 +60,7 @@ jQuery.extend({
 		text = text.replace( /Ώ/g, "Ω" );
 		text = text.replace( /ώ/g, "ω" );
 
-		elem.innerHTML = text;
+		(elem.tagName.toLowerCase() == "input") ? (elem.value = text) : (elem.innerHTML = text);
 	}
 });
 
